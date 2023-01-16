e40bb9be7939f73a9154d7adba059787bccaf947
Kade <mouseku43@gmail.com>
2023-01-16 23:32:25 +0900

    Add CAGR, Win_Rate

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ import numpy as np
 tickers = {"upbit": "KRW-BTC",
            "binance": "BTC/USDT"}
 
-def get_ohlcv(exchange='upbit', ticker = 'KRW-BTC', n=1, time=None): #코인의 정보를 불러옴, 500*n개의 정보
+def get_ohlcv(exchange='upbit', ticker = 'KRW-BTC', n=1, target_time=None): #코인의 정보를 불러옴, 500*n개의 정보
     dfs = [ ]
     if exchange=='upbit':
-        df = pyupbit.get_ohlcv(ticker, interval="minute240")
+        df = pyupbit.get_ohlcv(ticker, interval="minute240", to=target_time)
         dfs.append(df)
         for i in range(n):
             df = pyupbit.get_ohlcv(ticker, interval="minute240", to = df.index[0]) #to: 출력할 max date time을 지정
@@ -20,7 +20,7 @@ def get_ohlcv(exchange='upbit', ticker = 'KRW-BTC', n=1, time=None): #코인의 
     
     elif exchange=='binance':
         binance = ccxt.binance()
-        if time is None:
+        if target_time is None:
             last = datetime.datetime.fromtimestamp(binance.milliseconds()/1000)
         else:
             last = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
@@ -34,7 +34,6 @@ def get_ohlcv(exchange='upbit', ticker = 'KRW-BTC', n=1, time=None): #코인의 
         dfs.append(df)
         for i in range(n-1):
             since = binance.parse8601(datetime.datetime.strftime(df.index[-1], '%Y-%m-%d %H:%M:%S'))
-            print(f'df.index[-1] : {df.index[-1]}')
             df = binance.fetch_ohlcv(ticker, '4h', since=since, limit=1000)
             df = pd.DataFrame(df, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
             df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
@@ -52,17 +51,27 @@ def get_mdd(ror: list, window=20):
     dd = (ror - maximum) / maximum
     mdd = dd.min()
 
-    return mdd, dd
+    return mdd*100, dd
 
 def hpr(ror: list):
     final_ror = ror[-1]
     return (final_ror - 1) / 1
 
 def Ahpr(ror: list, period: float):
-    return (1+hpr(ror))**(1/period)-1
+    return ((1+hpr(ror))**(1/period)-1)*100
 
-def get_period(index: pd.DatetimeIndex):
-    return (index[-1]-index[0]).days
+def tdelta2year(index: pd.DatetimeIndex):
+    return (index[-1]-index[0]).days/365.2425
 
-# df = get_ohlcv('binance', tickers['binance'], 5)
+# 연평균 복리수익률
+def cagr(ror: list, period: float):
+    return ((ror[-1]/1)**(1/period)-1)*100
+
+def win_rate(win_rate: list):
+    num_trade = len(win_rate)
+    win_trade = win_rate.count(1)
+    return win_trade / num_trade * 100
+
+# df = get_ohlcv('binance', tickers['binance'], 4)
+# print(get_period(df.index))
 # df.to_excel('BINANCE_BTC_4h.xlsx', index=True)
